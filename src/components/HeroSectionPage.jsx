5ae812863c74190570c797bef8e474c6a6cdd233
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2021-11-08 20:36:38 +0100

    some style and animations added to hero section

--- src/components/HeroSectionPage.jsx
+++ src/components/HeroSectionPage.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import retro from "../Images/retro.jpg";
 
 export const HeroSectionPage = () => {
   return (
     <div className="hero-section">
-      <h1 className="p-5 text-center ">Hero Section</h1>
+      <div className="left-side">
+        <div className="hero-left-up">
+          <div className="bg-image"></div>
+          <div className="bg-text">
+            <h1 className="name">Pedro Rivas</h1>
+            <p className="pro">SoftWare Developer</p>
+          </div>
+        </div>
+        <div className="hero-left-down d-flex justify-content-around hero-left-right animate__animated animate__slideInUp">
+          <div className="background box">Background</div>
+          <div className="tech box">Tech stack</div>
+          <div className="projects box">Go to Projects</div>
+        </div>
+      </div>
+
+      <div className="hero-left-right animate__animated animate__slideInRight">
+        <h2 className="name">About Me</h2>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,29 @@ import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand">
+    <nav className="navbar navbar-expand ">
       <div className="container-fluid">
         <div className="logo">
-          <h3 className="name">
-            P<span className="letter">e</span>dro Riv
-            <span className="letter">a</span>s
-            <i className="fab fa-angellist"></i>
+          <h3 className="name animate__animated animate__heartBeat">
+            P<span className="letter">edr</span>o R
+            <span className="letter">iva</span>s
+            <i className="fab fa-angellist letter"></i>
           </h3>
         </div>
       </div>
-      <div className="navbar-nav">
-        <NavLink to="/" className="nav-link" exact activeClassName="my-active">
+      <div className="navbar-nav animate__animated animate_zoomIn">
+        <NavLink
+          to="/"
+          className="nav-link animate__animated animate__slideInLeft"
+          exact
+          activeClassName="my-active"
+        >
           Home
         </NavLink>
 
         <NavLink
           to="/projects"
-          className="nav-link"
+          className="nav-link animate__animated animate__bounce"
           exact
           activeClassName="my-active"
         >
@@ -28,7 +33,7 @@ export const Navbar = () => {
         </NavLink>
         <NavLink
           to="/contact"
-          className="nav-link"
+          className="nav-link animate__animated animate__jackInTheBox"
           exact
           activeClassName="my-active"
         >
@@ -36,7 +41,7 @@ export const Navbar = () => {
         </NavLink>
         <NavLink
           to="/blog"
-          className="nav-link"
+          className="nav-link animate__animated animate__slideInRight"
           exact
           activeClassName="my-active"
         >
