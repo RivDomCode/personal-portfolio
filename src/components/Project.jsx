ba0322259e343372ec465b4597c539cefed3137e
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2021-11-28 22:44:41 +0100

    project-section and project-component created

---
+++ src/components/Project.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Project = () => {
+  return (
+    <div className="Project-container">
+      <h1>one project</h1>
+      <p>gsegsedgsgsegsdgs</p>
+    </div>
+  );
+};
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,17 @@
 import React from "react";
+import { Project } from "./Project";
 
 export const Projects = () => {
-  const projects = [1, 2, 3, 4, 5];
+  const projects = [1, 2, 3, 4, 5, 6, 7, 8];
+
   return (
     <section className="projects-section">
       <h1 className="projects-title">Projects</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quia iste
-        ipsa! Neque dicta praesentium pariatur ducimus ab possimus numquam
-        sapiente mollitia, accusantium repudiandae. Reprehenderit quis mollitia
-        cumque quos debitis laborum illo consequatur labore tenetur tempora
-        nostrum illum velit corrupti, dolores ullam! Pariatur minus libero
-        aspernatur quis explicabo quas illo!
-      </p>
+      <div>
+        {projects.map((value) => (
+          <Project />
+        ))}
+      </div>
     </section>
   );
 };
