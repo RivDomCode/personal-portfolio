2e7efb43d886c1d4ea5250c0ff1a356bc255e44d
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2021-11-29 21:46:24 +0100

    started project section, still style nedeed

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,24 @@ import React from "react";
 export const Project = () => {
   return (
     <div className="project-card">
-      <div className="project-card-top"></div>
+      <div className="project-card-top">
+        <div className="project-card-top-img">Aqui va</div>
+        <div className="project-card-top-text">
+          <h3>Project Title</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+            excepturi ipsam minus inventore id assumenda doloribus, eos
+            voluptatibus magni nam rem, consequuntur consectetur, voluptate
+            nisi. Odit corrupti aperiam, facilis sapiente excepturi voluptas
+            mollitia ducimus, quas optio deleniti praesentium obcaecati dolore.
+            Obcaecati numquam et eos totam non vero dolorum voluptate nobis!
+          </p>
+          <button className="visit-btn">Visit</button>
+          <button className="more-info-btn">Read more</button>
+        </div>
+      </div>
+      Social
+      <div className="project-card-bottom"></div>
     </div>
   );
 };
