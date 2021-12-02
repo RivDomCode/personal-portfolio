893f421da03a7951b9101a4e3851d9815a5911e4
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2021-12-02 19:19:49 +0100

    contact-form design done

--- src/components/ContactMePage.jsx
+++ src/components/ContactMePage.jsx
@@ -2,8 +2,32 @@ import React from "react";
 
 export const ContactMePage = () => {
   return (
-    <div>
-      <h1>Contact me </h1>
-    </div>
+    <section className="contact">
+      <h1 className="contact-title">
+        L<span className="letter-e">e</span>t's get in touch
+      </h1>
+      <div className="contact-form-container">
+        <form className="contact-form">
+          <div className="input-container">
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+
+          <div className="input-container">
+            <label htmlFor="">Email</label>
+            <input type="text" />
+          </div>
+
+          <div className="input-container">
+            <label htmlFor="">Comments</label>
+            <textarea type="text" />
+          </div>
+
+          <div className="sub-btn-container">
+            <button className="sub-btn">Submit</button>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
