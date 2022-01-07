32989df8d09c67b92559eb5b16a7ef8946aed1fd
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-01-07 19:56:57 +0100

    responsive design changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,41 +5,41 @@ import Fade from "react-reveal/Zoom";
 export const Project = () => {
   return (
     <>
-      <Fade>
-        <div className="project-card">
-          <div className="project-card-top">
-            <div className="project-card-top-img">
-              <img
-                src="https://cdn.pixabay.com/photo/2019/04/07/08/56/network-4109223_960_720.jpg"
-                alt=""
-                className="img-fluid"
-              />
-            </div>
-            <div className="project-card-top-text">
-              <h3>Project Title</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
-                excepturi ipsam minus inventore id assumenda doloribus, eos
-                voluptatibus magni nam rem, consequuntur consectetur, voluptate
-                nisi. Odit corrupti aperiam, facilis sapiente excepturi voluptas
-                dolore. Obcaecati numquam et eos totam non vero dolorum
-                voluptate nobis!
-              </p>
-              <p className="icons">
-                <SiHtml5 className="icon" />
-                <SiSass className="icon" />
-                <SiCss3 className="icon" />
-                <SiReact className="icon" />
-                <SiFirebase className="icon" />
-              </p>
-              <div className="project-card-top-btn-container">
-                <button className="visit-btn">Go to Project</button>
-                <button className="more-info-btn">Read more</button>
-              </div>
+      {/* <Fade> */}
+      <div className="project-card">
+        <div className="project-card-top">
+          <div className="project-card-top-img">
+            <img
+              src="https://cdn.pixabay.com/photo/2019/04/07/08/56/network-4109223_960_720.jpg"
+              alt=""
+              className="img-fluid"
+            />
+          </div>
+          <div className="project-card-top-text">
+            <h3>Project Title</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+              excepturi ipsam minus inventore id assumenda doloribus, eos
+              voluptatibus magni nam rem, consequuntur consectetur, voluptate
+              nisi. Odit corrupti aperiam, facilis sapiente excepturi voluptas
+              dolore. Obcaecati numquam et eos totam non vero dolorum voluptate
+              nobis!
+            </p>
+            <p className="icons">
+              <SiHtml5 className="icon" />
+              <SiSass className="icon" />
+              <SiCss3 className="icon" />
+              <SiReact className="icon" />
+              <SiFirebase className="icon" />
+            </p>
+            <div className="project-card-top-btn-container">
+              <button className="visit-btn">Go to Project</button>
+              <button className="more-info-btn">Read more</button>
             </div>
           </div>
         </div>
-      </Fade>
+      </div>
+      {/* </Fade> */}
     </>
   );
 };
