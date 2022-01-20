627bd3dba0336bf9582ec7ebd5e62c562e1e49f8
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-01-20 20:04:07 +0100

    about me section done

--- src/components/AboutMePage.jsx
+++ src/components/AboutMePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
 
 export const AboutMePage = () => {
   return (
@@ -15,10 +16,18 @@ export const AboutMePage = () => {
               learner.
             </li>
             <li>
-              I am problem solver oriented guy who believes the language is just
-              a tool to get a job done...just try to choose the right one for
-              each project!
+              I speak spanish and english because I was living in the UK for
+              nearly 4 years
             </li>
+            <li>
+              I have worked with JavaScript, React, Flutter, React Native,
+              NodeJS....
+            </li>
+            <li>
+              ...but I am problem solving oriented guy who believes the
+              language/framework is just a tool to get a job done...
+            </li>
+            <li>...just try to choose the right one for each project!</li>
           </ul>
           <h2>What do i do?</h2>
           <ul>
@@ -28,6 +37,37 @@ export const AboutMePage = () => {
             <li>Web Design</li>
             <li>SEO</li>
             <li>...</li>
+          </ul>
+          <h2>Wanna know more?</h2>
+          <ul className="wanna-list">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/pedrorivasdominguez/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaLinkedin /> Linkedin
+              </a>
+            </li>
+            <li className="github">
+              <a
+                href="https://github.com/RivDomCode"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithubSquare /> GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:rivdomdev@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+                className="email"
+              >
+                rivdomdev@gmail.com
+              </a>
+            </li>
           </ul>
         </div>
       </div>
