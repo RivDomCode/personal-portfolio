2c64c8482b8e4b3e23ffb093759525aa39d4510d
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-04-08 12:53:57 +0200

    df

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,19 +3,19 @@ import { useEffect } from "react/cjs/react.development";
 import { ContactMePage } from "./ContactMePage";
 import { HeroSectionPage } from "./HeroSectionPage";
 import { Projects } from "./Projects";
-// import Swal from "sweetalert2";
-// import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 export const Home = () => {
-  // useEffect(() => {
-  //   const MySwal = withReactContent(Swal);
-  //   return MySwal.fire({
-  //     icon: "warning",
-  //     title: "Currently this porfolio is being updated...",
-  //     text: "Not all sections are available, but you can enjoy most of them!",
-  //     confirmButtonColor: "	#2e74d6",
-  //   });
-  // }, []);
+  useEffect(() => {
+    const MySwal = withReactContent(Swal);
+    return MySwal.fire({
+      icon: "warning",
+      title: "Currently this porfolio is being updated...",
+      text: "Not all sections are available, but you can enjoy most of them!",
+      confirmButtonColor: "	#2e74d6",
+    });
+  }, []);
 
   return (
     <>
